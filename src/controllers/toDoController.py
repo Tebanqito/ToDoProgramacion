d20c6b9142159c70eab0d553ade0c89fc636484b
yemesesteban@gmail.com <yemesesteban@gmail.com>
2025-10-15 20:53:04 -0300

    [feat/Yemes] - feat: createToDo with post method already done

--- src/controllers/toDoController.py
+++ src/controllers/toDoController.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from src.schemas.toDoSchema import toDoCreate, toDoOut, toDoUpdate
 from src.database.dbconfig import dbDependency
+from src.services.toDoService import createNewToDo
 
 router = APIRouter(
-    prefix="/products",
-    tags=["products"],
+    prefix="/toDo´s",
+    tags=["toDo´s"],
 )
+
+@router.post("/create", response_model=toDoOut, status_code=201)
+async def createToDo(db: dbDependency, payload: toDoCreate):
+    return createNewToDo(db, payload)
